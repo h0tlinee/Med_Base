2263157d9709294d6a4d747ee68a80a53cc87396
h0tlinee <kulakov003@gmail.com>
2023-11-24 01:41:31 +0700

    добавил карточку товара(картинка и основные поля)

--- app.py
+++ app.py
@@ -6,10 +6,21 @@ from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
 
+items_info=[{'code':'1','price':'100','description':'good','structure':'hz'},
+            {'code':'2','price':'200','description':'good1','structure':'hz1'},
+            {'code':'3','price':'300','description':'good2','structure':'hz2'}
+            
+            
+            ]
+
+
+
 @app.route('/')
 def main():
-    return render_template('main.html')
-    
+    return render_template('main.html',
+        item_info=items_info            
+    )
+
     
 if __name__ == "__main__":
     app.run(debug=True)
